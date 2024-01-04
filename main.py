a7ac048ed397a651329b295ba3ea3c5ec9347684
Miikka Virtanen <32563239+MiiVirt@users.noreply.github.com>
2024-01-04 13:37:51 +0200

    Add files via upload
    unicode will now loop so size of the key does not matter

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import numpy as np
 
+file_path = "data.txt"
+
 #TODO Handling if less than 32 or over 126 (loop unicode)
 #TODO Error handling for empty input and negative keys
 #TODO menu inprovement
 #TODO function that converts to unicode/char
 #TODO Key value can be anything and will be converted into sum of unicode
+
+def Unicode(value):
+    #loop unicode if it goes bellow 32
+    if value < 32:
+        return 126 - (31 - value) % 95
+    elif value > 126:
+        return 32 + (value - 127) % 95
+    else:
+        return value
 def Encrypting():
 
     data = input("Data")
 
     #Check if key is int
     while True:
-        x = input("Key (number)")
         try:
-            key = int(x)
+            key = int(input("Key (number)"))
             break
         except ValueError:
             print("Invalid input. Enter a round number as key")
@@ -24,35 +34,25 @@ def Encrypting():
     #Convert key to integrer
     intkey = int(key)
 
-    #Encryption
-    encrypted = datalist - intkey
-
-    #Convert to encrypted data
-    encrypted_data = ''.join([chr(element) for element in encrypted])
+    # Encryption and loop Unicode values within printable ASCII range
+    encrypted_data = ''.join([chr(Unicode(value - intkey)) for value in datalist])
 
     #Save the data to a data.txt file
-    file_path = "data.txt"
     with open(file_path, 'w') as file:
         file.write(encrypted_data)
 
-    #Debuggind
-    #print(data)
-    #print(encrypted)
-
-    print(encrypted_data)
+    print("Encrypted data", encrypted_data)
 
 def Encoding():
 
     #data = input("Encrypted data")
-    file_path = "data.txt"
     with open(file_path, 'r') as file:
         data = file.read()
 
     #Check if key is int
     while True:
-        x = input("Key (number)")
         try:
-            key = int(x)
+            key = int(input("Key (number)"))
             break
         except ValueError:
             print("Invalid input. Enter a round number as key")
@@ -63,13 +63,10 @@ def Encoding():
     #Convert key to integerer
     intkey = int(key)
 
-    #Encoding
-    encoded = datalist + intkey
+    # Loop Unicode values within printable ASCII range
+    decoded_data = ''.join([chr(Unicode(value + intkey)) for value in datalist])
 
-    #Convert data back to characters
-    converted = ''.join([chr(element) for element in encoded])
-
-    print(converted)
+    print("Decrypted data", decoded_data)
 
 def Main():
     print("Are you trying to encrypt your data or encode your encrypted data?")
